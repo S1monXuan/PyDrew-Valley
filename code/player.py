2e36f98ef824216df05ec1ec65c287b1bfc10309
SimonXuan <xxmspk@163.com>
2024-12-22 18:43:12 -0500

    realize basic movement

---
+++ code/player.py
@@ -0,0 +1,48 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+
+        # general set up
+        self.image = pygame.Surface((64, 32))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(center = pos)
+    
+        # Movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        print(self.direction)
+
+    def move(self, dt):
+        # normaliziing a vector
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+        
+        
+        self.pos += self.direction * self.speed * dt
+        self.rect.center = self.pos
+    
+    def update(self, dt):
+        self.input()
+        self.move(dt)
