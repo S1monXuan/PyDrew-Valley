878fe34a871d0cd104585a3b0eb69d7ae2c2141e
SimonXuan <xxmspk@163.com>
2024-12-22 21:53:16 -0500

    import player img and status

--- code/player.py
+++ code/player.py
@@ -1,19 +1,35 @@
 import pygame
 from settings import *
+from support import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
         super().__init__(group)
 
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+
         # general set up
-        self.image = pygame.Surface((64, 32))
-        self.image.fill('green')
+        self.image = self.animations[self.status][self.frame_index]
+        # self.image.fill('green')
         self.rect = self.image.get_rect(center = pos)
     
         # Movement attributes
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+    def import_assets(self):
+        self.animations = {'up': [],'down': [],'left': [],'right': [],
+                           'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+                           'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
+                           'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
+                           'right_water':[],'left_water':[],'up_water':[],'down_water':[]}
+
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -32,7 +48,7 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-        print(self.direction)
+        # print(self.direction)
 
     def move(self, dt):
         # normaliziing a vector
@@ -41,11 +57,11 @@ class Player(pygame.sprite.Sprite):
         
         # horizontal movement
         self.pos.x += self.direction.x * self.speed * dt 
-        self.rect.centerx = self.pos
+        self.rect.centerx = self.pos.x
 
         # vertical movement
         self.pos.y += self.direction.y * self.speed * dt
-        self.rect.centery = self.pos
+        self.rect.centery = self.pos.y
     
     def update(self, dt):
         self.input()
